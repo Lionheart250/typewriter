bc43b282855caeb4c425927d879884894eae9aea
vagrant <jacksonlionheart@gmail.com>
2023-07-04 17:49:27 +0000

    first

---
+++ hello_timeout.js
@@ -0,0 +1,13 @@
+setTimeout (() => {
+process.stdout.write('43110\n');
+}, 1000);
+setTimeout (() => {
+  process.stdout.write('th3r3\n');
+}, 2000)
+setTimeout (() => {
+  process.stdout.write('w0rld\n');
+}, 3000)
+
+console.log(setTimeout);
+
+
